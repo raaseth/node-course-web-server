7f102c8a6f2acd22e8502d8c82653e77858997d5
Mohamed Lamine SIAD <m.la.siad@gmail.com>
2018-09-16 15:58:08 +0100

    Adding portfolio page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/portfolio', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Portfolio Page'
+    });
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
